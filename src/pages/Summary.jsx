ad322d29b441af22146eb7de2bb7d688a5330c1f
stekdev <stefanox0023@gmail.com>
2024-11-17 20:30:12 +0100

    added back btn to navigate home

--- src/pages/Summary.jsx
+++ src/pages/Summary.jsx
@@ -1,14 +1,25 @@
 import { useContext } from 'react';
 import { BookingContext } from '../store/booking-context.jsx';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function SummaryPage() {
 
   const bookingCtx = useContext(BookingContext);
+  const navigate = useNavigate();
+
+  function goToHome() {
+    navigate('/');
+  }
 
   return(
     <>
       <p>Summary Page Component Works!</p>
+
+      <section className='m-5'>
+        <button onClick={() => goToHome()} className="btn btn-primary">Back</button>
+      </section>
+
       <div className='p-5' style={{ border: '2px solid red' }}>
         {JSON.stringify(bookingCtx.formDataState)}
       </div>
